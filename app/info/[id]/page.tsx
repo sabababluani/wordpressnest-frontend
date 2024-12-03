3b245df8f8f875d52ce8762a481cb3cede2cca92
alexandre879 <aleksiapkhazava@gmail.com>
2024-12-03 05:47:57 +0400

    feat: fixed-problem

--- app/info/[id]/page.tsx
+++ app/info/[id]/page.tsx
@@ -73,10 +73,10 @@ const Info = (): JSX.Element => {
   if (portError) return <div>Error loading data...</div>;
 
   if (!username) return <div>Loading...</div>;
-  if (usernameError) return <div>Error loading data...</div>;
+  // if (usernameError) return <div>Error loading data...</div>;
 
   if (!database) return <div>Loading...</div>;
-  if (databaseError) return <div>Error loading data...</div>;
+  // if (databaseError) return <div>Error loading data...</div>;
 
   return (
     <div className={styles.mainContainer}>
