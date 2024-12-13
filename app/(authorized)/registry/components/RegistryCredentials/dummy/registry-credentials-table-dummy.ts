f3efc46734df591426b1a6cc78d8c073732ad19a
sabababluani <158265615+sabababluani@users.noreply.github.com>
2024-12-13 16:55:33 +0400

    feat: registry table

---
+++ app/(authorized)/registry/components/RegistryCredentials/dummy/registry-credentials-table-dummy.ts
@@ -0,0 +1,14 @@
+export const RegistryTableDummy = [
+  {
+    key: '1',
+    name: 'Registry 1',
+    registry: 'Docker Hub',
+    created: '2024-08-28',
+  },
+  {
+    key: '2',
+    name: 'Registry 2',
+    registry: 'GitHub Packages',
+    created: '2024-08-27',
+  },
+];
