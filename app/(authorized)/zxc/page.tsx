09ffda6b7514168e2229f9ccc3438785b5702658
alexandre879 <aleksiapkhazava@gmail.com>
2024-12-15 23:27:12 +0400

    feat: created another modal

--- app/(authorized)/zxc/page.tsx
+++ app/(authorized)/zxc/page.tsx
@@ -1,11 +1,11 @@
 import LabelModal from "../wpsites/components/LabelModal/LabelModal";
 import UpdateModal from "../wpsites/components/UpdateModal/UpdateModal";
+import UpdateThemesModal from "../wpsites/components/UpdateThemesModal/UpdateThemesModal";
 
 
 export default function () {
     return (
         <>
-            
         </>
     )
 }
