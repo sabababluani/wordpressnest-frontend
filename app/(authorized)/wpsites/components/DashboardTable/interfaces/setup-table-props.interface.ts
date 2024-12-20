3387b68eee08f1305befc3179af96d6b2848f91f
sabaabramishvili <157946594+sabaabramishvili@users.noreply.github.com>
2024-12-20 21:11:22 +0400

    refactor: connect wpsites dashboard

---
+++ app/(authorized)/wpsites/components/DashboardTable/interfaces/setup-table-props.interface.ts
@@ -0,0 +1,13 @@
+export interface SetupTablePropsInterface {
+  phpVersion: string;
+  siteTitle: string;
+  key: React.Key;
+  status: string;
+  visit: string;
+  bandwidth: string;
+  diskUsage: string;
+  wpVersion: string;
+  podName: string;
+  nameSpace: string;
+  id: number;
+}
