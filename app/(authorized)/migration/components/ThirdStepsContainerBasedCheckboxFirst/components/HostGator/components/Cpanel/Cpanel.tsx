c7b827c50cb4b6b35dd22366eff03015d490f4f5
alexandre879 <aleksiapkhazava@gmail.com>
2024-12-21 21:56:13 +0400

    feat: added another selection

---
+++ app/(authorized)/migration/components/ThirdStepsContainerBasedCheckboxFirst/components/HostGator/components/Cpanel/Cpanel.tsx
@@ -0,0 +1,14 @@
+
+import InputAndLabel from "../../../DigitalOcean/components/InputAndLabel/InputAndLabel";
+import styles from "./Cpanel.module.scss";
+
+const Cpanel = () => {
+    return (
+        <div className={styles.mainWrapper} >
+            <span>You will need to enable SSH access</span>
+            <InputAndLabel firstColumnActive strechedInput firstColumnLabel={'cPanel URL'} />
+        </div>
+    )
+}
+
+export default Cpanel;
---
+++ app/(authorized)/migration/components/ThirdStepsContainerBasedCheckboxFirst/components/HostGator/components/Sftp/Sftp.tsx
@@ -0,0 +1,29 @@
+import InputAndLabel from '../../../DigitalOcean/components/InputAndLabel/InputAndLabel';
+import styles from './Sftp.module.scss';
+
+const Sftp = () => {
+  return (
+    <div className={styles.mainWrapper}>
+      <span>You will need to enable SSH access</span>
+      <InputAndLabel
+        firstColumnActive
+        firstColumnLabel={'Server address'}
+        secondColumnActive
+        secondColumnLabel={'Port'}
+      />
+      <InputAndLabel
+        firstColumnActive
+        firstColumnLabel={'SFTP Username'}
+        secondColumnActive
+        secondColumnLabel={'SFTP Password'}
+      />
+      <InputAndLabel
+        firstColumnActive
+        firstColumnLabel={'WordPress site path'}
+        strechedInput
+      />
+    </div>
+  );
+};
+
+export default Sftp;
