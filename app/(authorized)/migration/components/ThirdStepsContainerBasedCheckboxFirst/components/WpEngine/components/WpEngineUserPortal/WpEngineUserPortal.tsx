438a866616e8dc3f6d240c65a696783518472df7
alexandre879 <aleksiapkhazava@gmail.com>
2024-12-22 17:46:54 +0400

    feat: created another selectio field

--- app/(authorized)/migration/components/ThirdStepsContainerBasedCheckboxFirst/components/WpEngine/components/WpEngineUserPortal/WpEngineUserPortal.tsx
+++ app/(authorized)/migration/components/ThirdStepsContainerBasedCheckboxFirst/components/WpEngine/components/WpEngineUserPortal/WpEngineUserPortal.tsx
@@ -1,4 +1,39 @@
+import { Checkbox } from 'antd';
+import styles from './WpEngineUserPortal.module.scss';
+import { useState } from 'react';
+import InputAndLabel from '../../../DigitalOcean/components/InputAndLabel/InputAndLabel';
+
 const WpEngineUserPortal = () => {
-  return <div></div>;
+  const [activeCheckbox, setActiveCheckbox] = useState<boolean>(false);
+
+  const handleCheckboxClick = () => {
+    setActiveCheckbox((prev: boolean) => !prev);
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <span className={styles.mainCaptionStyle}>
+        We recommend adding hosting as a user on your WP engine account. When
+        providing your username and password to login, this will require
+        additonal security checks, and thi magration process can take
+        considerably longer.
+      </span>
+      <div className={styles.forCheckboxAndCaption}>
+        <div className={styles.checkboxWrapper}>
+          <Checkbox onClick={handleCheckboxClick} checked={activeCheckbox} />
+        </div>
+        <span className={styles.checkCaptionStyle}>
+          Check here if you would like us to preceed without giving us full
+          access
+        </span>
+      </div>
+      <InputAndLabel
+        firstColumnActive
+        firstColumnLabel={'WP Engine User Portal username'}
+        secondColumnActive
+        secondColumnLabel={'WP Engine User Portal Password'}
+      />
+    </div>
+  );
 };
 export default WpEngineUserPortal;
